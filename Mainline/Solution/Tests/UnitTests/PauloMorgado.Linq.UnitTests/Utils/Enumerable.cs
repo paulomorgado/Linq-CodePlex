f647e335be748928e70c3446d1c8c385742e8f13
Paulo Morgado <paulomorgado@users.noreply.github.com>
2010-10-13 23:29:44 +0000

    Added EnumerableAsCollection for the tests

---
+++ Mainline/Solution/Tests/UnitTests/PauloMorgado.Linq.UnitTests/Utils/Enumerable.cs
@@ -0,0 +1,57 @@
+﻿namespace PauloMorgado.Linq.UnitTests.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections;
+
+    public static class Enumerable
+    {
+        public static ICollection AsCollection(this IEnumerable source)
+        {
+            return new EnumerableAsCollection(source);
+        }
+
+        private class EnumerableAsCollection : ICollection
+        {
+            private IEnumerable source;
+
+            public EnumerableAsCollection(IEnumerable source)
+            {
+                this.source = source;
+            }
+
+            #region IEnumerable Members
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return this.source.GetEnumerator();
+            }
+
+            #endregion
+
+            #region ICollection Members
+
+            public int Count
+            {
+                get { return 0; }
+            }
+
+            public void CopyTo(Array array, int index)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsSynchronized
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public object SyncRoot
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            #endregion
+        }
+    }
+}
